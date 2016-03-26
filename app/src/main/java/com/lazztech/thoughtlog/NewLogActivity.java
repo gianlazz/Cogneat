b91a75d4921177552282692fbcff5a6fa8dc43b2
Gian Lazzarini <gianlazzarini@gmail.com>
2016-03-26 14:53:59 -0700

    Removed hardcoded path to sd card and changed the checkbox onclick to a String.
    http://stackoverflow.com/questions/32594444/checkbox-append-text-to-string-if-checked-then-write-either-empty-string-or-appe
    
    "I'm assuming that you have the CheckBoxes working as the code looks like will work. I will warn you that how you currently have it you will be appending the text every that you check and uncheck the checkBox as the append is never undone. I believe your current code was not working because you never actually called the print command for the FileOutStream so you added data but never printed it
    
    For writing to the SD card couple of things that I would recommend(In code below). Instead of using the OutStreamWriter use a StringBuffer for appending the text to and a PrintStream to write to file as this allows to write Strings to the file. It's also poor practice to hardcode where the SD card is and you should use Environment.getExternalStorageDirectory() rather than "/sdcard""

--- app/src/main/java/com/lazztech/thoughtlog/NewLogActivity.java
+++ app/src/main/java/com/lazztech/thoughtlog/NewLogActivity.java
@@ -26,12 +26,21 @@ public class NewLogActivity extends Activity
 	CheckBox overestimating;
 	CheckBox catastrophizing;
 	CheckBox overgeneralizing;
-	StringBuffer distortions;
+
+	String distortions;
+	String fortuneTellingString = "Fortune-Telling";
+	String mindReadingString = "Mind-Reading";
+	String labelingString = "Labeling";
+	String filteringString = "Filtering";
+	String overestimatingString = "Overestimating";
+	String catastrophizingString = "Catastrophizing";
+	String OvergeneralizingString = "Overgeneralizing";
+
 	EditText altbehavior;
 	EditText altthoughts;
 	Button btnWriteSDFile;
 	/** Called when the activity is first created. */
-	
+
 	@Override
 	public void onCreate(Bundle savedInstanceState)
 	{
@@ -44,7 +53,7 @@ public class NewLogActivity extends Activity
 		thoughts = (EditText) findViewById(R.id.thoughts);
 		emotions = (EditText) findViewById(R.id.emotions);
 		behavior = (EditText) findViewById(R.id.behavior);
-		
+
 		fortunetelling = (CheckBox) findViewById(R.id.fortunetellingCheckBox1);
 		mindreading = (CheckBox) findViewById(R.id.mindreadingCheckBox2);
 		labeling = (CheckBox) findViewById(R.id.labelingCheckBox3);
@@ -52,15 +61,15 @@ public class NewLogActivity extends Activity
 		overestimating = (CheckBox) findViewById(R.id.overestimatingCheckBox5);
 		catastrophizing = (CheckBox) findViewById(R.id.catastrophizingCheckBox6);
 		overgeneralizing = (CheckBox) findViewById(R.id.overgeneralizingCheckBox7);
-		
+
 		altbehavior = (EditText) findViewById(R.id.altbehaviors);
 		altthoughts = (EditText) findViewById(R.id.altthoughts);
 
 		btnWriteSDFile = (Button) findViewById(R.id.btnWriteSDFile);
 	}
-		
+
 		//Checkbox onclick actions
-	public void onCheckboxClicked(View view) 
+	public void onCheckboxClicked(View view)
 	{
 
 		// Is the view now checked?
@@ -70,27 +79,38 @@ public class NewLogActivity extends Activity
 		switch(view.getId()) {
 			case R.id.fortunetellingCheckBox1:
 				if (checked)
-				distortions.append("Fortune-Telling").toString();
+					distortions = + fortuneTellingString;
+				//else
+				//	distortions = "";
 				break;
-			//case R.id.mindreadingCheckBox2:
-				//if (checked)
-                // Append "Mind-Reading" to String distortions
-				//break;
-				// TODO: Veggie sandwi
+			case R.id.mindreadingCheckBox2:
+				if (checked)
+                 	distortions = "Mind-Reading";
+				break;
+
 		}
 		btnWriteSDFile.setOnClickListener(new OnClickListener() {
-			
+
 				public void onClick(View v) {
 					// write on SD card file data in the text box
 					try {
-						File myFile = new File("/sdcard/mythoughtlog.txt");
-						myFile.createNewFile();
-						FileOutputStream fOut = new FileOutputStream(myFile, true);
-						
-						//Open the printStream to allow for Strings to be written
-						PrintStream printStream = new PrintStream(fOut);
-						
-						
+						File directory = Environment.getExternalStorageDirectory();
+						File myFile = new File(directory, "mythoughtlog.txt");
+
+						// Check if the file already exists so you don't keep creating
+		        if(!myFile.exists()) {
+		            //Log.i(TAG, "Creating the file as it doesn't exist already");
+		            myFile.createNewFile();
+		        }
+
+						// Open the FileoutputStream
+		        FileOutputStream fOut = new FileOutputStream(myFile, true);
+
+						// Open the printStream to allow for Strings to be written
+		        PrintStream printStream = new PrintStream(fOut);
+
+
+						// Using a stringBuffer to append all the values to
 						StringBuffer stringBuffer = new StringBuffer();
 						stringBuffer.append(txtData.getText());
 						stringBuffer.append('\n');
@@ -109,7 +129,11 @@ public class NewLogActivity extends Activity
 						stringBuffer.append('\n');
 						stringBuffer.append(altthoughts.getText());
 						stringBuffer.append('\n');
+
+						// Print the stringBuffer to the file
 						printStream.print(stringBuffer.toString());
+
+        		// Close everything out
 						printStream.close();
 						fOut.close();
 						txtData.setText("Name: "); //ClearScreen
@@ -127,19 +151,9 @@ public class NewLogActivity extends Activity
 						Toast.makeText(getBaseContext(), e.getMessage(),
 									   Toast.LENGTH_SHORT).show();
 					}
-					
-					
+
+
 			}//Save onClick
 		});// btnWriteSDFile
 	}
 }// AndSDcard
-	
-		
-		
-		
-
-
-	
-		
-
-		
