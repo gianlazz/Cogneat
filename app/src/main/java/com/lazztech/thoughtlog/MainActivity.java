df2738f4d2c89b61f1a537fb4bdd9364b373a47b
Gian Lazzarini <gianlazzarini@gmail.com>
2016-03-31 14:49:28 -0700

    Added Sidebar Menu Example Code.
    http://blog.teamtreehouse.com/add-navigation-drawer-android
    
    https://github.com/treehouse/android-navigation-drawer-final

--- app/src/main/java/com/lazztech/thoughtlog/MainActivity.java
+++ app/src/main/java/com/lazztech/thoughtlog/MainActivity.java
@@ -8,14 +8,26 @@ import android.widget.*;
 
 public class MainActivity extends Activity
 {
+	private ListView mDrawerList;
+	private ArrayAdapter<String> mAdapter;
+
     /** Called when the activity is first created. */
     @Override
-    public void onCreate(Bundle savedInstanceState)
-	{
-        super.onCreate(savedInstanceState);
+    public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
         setContentView(R.layout.main);
+
+		mDrawerList = (ListView)findViewById(R.id.navList);
+
+		addDrawerItems();
 	}
-	
+
+	private void addDrawerItems() {
+		String[] osArray = { "Log History", "New Log", "Analytics", "Settings"};
+		mAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, osArray);
+		mDrawerList.setAdapter(mAdapter);
+	}
+
 	public void onNewLogButtonClick(View view)
 	{
 		Intent intent = new Intent(this, NewLogActivity.class);
