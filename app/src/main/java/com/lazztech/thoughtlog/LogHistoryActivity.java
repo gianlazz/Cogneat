8257043b30b2ca18cf1f2bce4a80b978f914585a
Gian Lazzarini <gianlazzarini@gmail.com>
2016-07-25 19:54:04 -0700

    Removed New Log button & inflater menu code from LogHistoryActivity.java

--- app/src/main/java/com/lazztech/thoughtlog/LogHistoryActivity.java
+++ app/src/main/java/com/lazztech/thoughtlog/LogHistoryActivity.java
@@ -53,36 +53,4 @@ public class LogHistoryActivity extends AppCompatActivity
         return line;
     }
 
-
-    public void onNewLogButtonClick(View view)
-    {
-        Intent intent = new Intent(this, NewLogActivity.class);
-        startActivity(intent);
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu)
-    {
-        // Inflate main_menu.xml
-        MenuInflater inflater = getMenuInflater();
-        inflater.inflate(R.menu.main_menu, menu);
-        return true;
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item)
-    {
-        switch (item.getItemId())
-        {
-            case R.id.mainMenuAbout:
-                Toast.makeText(this, "This is my app!!!", Toast.LENGTH_SHORT).show();
-                return true;
-            case R.id.mainMenuExit:
-                finish();
-                return true;
-
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
 }
