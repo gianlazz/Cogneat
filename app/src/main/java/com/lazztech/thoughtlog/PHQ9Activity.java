d4831ac0bb38cf5d3ab46b004fa2bb886d876bbe
Gian Lazzarini <gianlazzarini@gmail.com>
2016-07-31 18:45:36 -0700

    Setup PHQ9Activity.java onCreate

--- app/src/main/java/com/lazztech/thoughtlog/PHQ9Activity.java
+++ app/src/main/java/com/lazztech/thoughtlog/PHQ9Activity.java
@@ -1,7 +1,19 @@
 package com.lazztech.thoughtlog;
 
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
 /**
  * Created by gianlazzarini on 7/31/16.
  */
-public class PHQ9Activity {
+public class PHQ9Activity extends AppCompatActivity
+{
+    /** Called when the activity is first created. */
+    @Override
+    public void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        //Set MainActivity.xml as user interface layout
+        setContentView(R.layout.phq9);
+        // bind GUI elements with local controls
+    }
 }
