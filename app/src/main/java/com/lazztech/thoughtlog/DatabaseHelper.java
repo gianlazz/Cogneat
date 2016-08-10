801ca37ba4dbaf3f09376ca7b7fbdffbcb01a30d
Gian Lazzarini <gianlazzarini@gmail.com>
2016-08-09 18:51:24 -0700

    Added getAllData method
    https://www.youtube.com/watch?v=KUq5wf3Mh0c

--- app/src/main/java/com/lazztech/thoughtlog/DatabaseHelper.java
+++ app/src/main/java/com/lazztech/thoughtlog/DatabaseHelper.java
@@ -2,6 +2,7 @@ package com.lazztech.thoughtlog;
 
 import android.content.ContentValues;
 import android.content.Context;
+import android.database.Cursor;
 import android.database.sqlite.SQLiteDatabase;
 import android.database.sqlite.SQLiteOpenHelper;
 
@@ -45,5 +46,11 @@ public class DatabaseHelper extends SQLiteOpenHelper {
             return false;
         else
             return true;
+    }
+
+    public Cursor getAllData(){
+        SQLiteDatabase db = this.getWritableDatabase();
+        Cursor res = db.rawQuery("select * from " +TABLE_NAME, null);
+        return res;
     }
 }
