27bf4195435f29777ab1156a77cbddf5a7e60128
Gian Lazzarini <gianlazzarini@gmail.com>
2016-08-10 22:42:36 -0700

    Adapted database columns to Thought Log fields.

--- app/src/main/java/com/lazztech/thoughtlog/DatabaseHelper.java
+++ app/src/main/java/com/lazztech/thoughtlog/DatabaseHelper.java
@@ -13,9 +13,13 @@ public class DatabaseHelper extends SQLiteOpenHelper {
     public static final String DATABASE_NAME = "Student.db";
     public static final String TABLE_NAME = "student_table";
     public static final String COL_1 = "_id";
-    public static final String COL_2 = "NAME";
-    public static final String COL_3 = "SURNAME";
-    public static final String COL_4 = "MARKS";
+    public static final String COL_2 = "SITUATION";
+    public static final String COL_3 = "THOUGHTS";
+    public static final String COL_4 = "EMOTIONS";
+    public static final String COL_5 = "BEHAVIOR";
+    public static final String COL_6 = "DISTORTIONS";
+    public static final String COL_7 = "ALTBEHAVIOR";
+    public static final String COL_8 = "ALTTHOUGHTS";
 
     public DatabaseHelper(Context context) {
         super(context, DATABASE_NAME, null, 1);
@@ -24,7 +28,7 @@ public class DatabaseHelper extends SQLiteOpenHelper {
 
     @Override
     public void onCreate(SQLiteDatabase db) {
-        db.execSQL("create table " + TABLE_NAME + " (_id INTEGER PRIMARY KEY AUTOINCREMENT, NAME TEXT, SURNAME TEXT, MARKS TEXT) ");
+        db.execSQL("create table " + TABLE_NAME + " (_id INTEGER PRIMARY KEY AUTOINCREMENT, SITUATION TEXT, THOUGHTS TEXT, EMOTIONS TEXT, BEHAVIOR TEXT, DISTORTIONS TEXT, ALTBEHAVIOR TEXT, ALTTHOUGHTS TEXT) ");
 
     }
 
@@ -35,12 +39,16 @@ public class DatabaseHelper extends SQLiteOpenHelper {
         onCreate(db);
     }
 
-    public boolean insertData(String name,String surname,String marks){
+    public boolean insertData(String situation,String thoughts,String emotions, String behavior, String distortions, String altbehavior, String altthoughts){
         SQLiteDatabase db = this.getWritableDatabase();
         ContentValues contentValues = new ContentValues();
-        contentValues.put(COL_2,name);
-        contentValues.put(COL_3,surname);
-        contentValues.put(COL_4,marks);
+        contentValues.put(COL_2,situation);
+        contentValues.put(COL_3,thoughts);
+        contentValues.put(COL_4,emotions);
+        contentValues.put(COL_5,behavior);
+        contentValues.put(COL_6,distortions);
+        contentValues.put(COL_7,altbehavior);
+        contentValues.put(COL_8,altthoughts);
         long result = db.insert(TABLE_NAME, null, contentValues);
         if(result == -1)
             return false;
