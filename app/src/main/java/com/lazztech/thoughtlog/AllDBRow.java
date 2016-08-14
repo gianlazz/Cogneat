9d01792944814da1bb08540c14a04f5f549cb8a1
Gian Lazzarini <gianlazzarini@gmail.com>
2016-08-13 21:52:58 -0700

    Created AllDBRow java class

---
+++ app/src/main/java/com/lazztech/thoughtlog/AllDBRow.java
@@ -0,0 +1,11 @@
+package com.lazztech.thoughtlog;
+
+import android.support.v7.app.AppCompatActivity;
+
+/**
+ * Created by gianlazzarini on 8/13/16.
+ */
+
+public class AllDBRow extends AppCompatActivity {
+
+}
