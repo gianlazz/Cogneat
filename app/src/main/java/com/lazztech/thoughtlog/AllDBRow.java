d3fe615d3948fca031abbf164be3dd45e2900aa3
Gian Lazzarini <gianlazzarini@gmail.com>
2016-08-13 22:32:26 -0700

    Declaired and attatched textview variables to layout id

--- app/src/main/java/com/lazztech/thoughtlog/AllDBRow.java
+++ app/src/main/java/com/lazztech/thoughtlog/AllDBRow.java
@@ -2,6 +2,7 @@ package com.lazztech.thoughtlog;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.widget.TextView;
 
 /**
  * Created by gianlazzarini on 8/13/16.
@@ -9,6 +10,16 @@ import android.support.v7.app.AppCompatActivity;
 
 public class AllDBRow extends AppCompatActivity {
     /** Called when the activity is first created. */
+
+    TextView id;
+    TextView situation;
+    TextView thoughts;
+    TextView emotions;
+    TextView behavior;
+    TextView distortions;
+    TextView altbehavior;
+    TextView altthoughts;
+
     @Override
     public void onCreate(Bundle savedInstanceState){
         super.onCreate(savedInstanceState);
@@ -17,6 +28,15 @@ public class AllDBRow extends AppCompatActivity {
         // bind GUI elements with local controls
 
         String data = getIntent().getExtras().getString("_id");
+
+        id = (TextView) findViewById(R.id.dbidTextView);
+        situation = (TextView) findViewById(R.id.situationTextView);
+        thoughts = (TextView) findViewById(R.id.thoughtsTextView);
+        emotions = (TextView) findViewById(R.id.emotionsTextView);
+        behavior = (TextView) findViewById(R.id.behaviorTextView);
+        distortions = (TextView) findViewById(R.id.distortionsTextView);
+        altbehavior = (TextView) findViewById(R.id.altbehaviorTextView);
+        altthoughts = (TextView) findViewById(R.id.altthoughsTextView);
 
     }
 }
