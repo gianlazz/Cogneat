6ceb458ccab512ce19e4f363ee70e3ebb9ad6306
Gian Lazzarini <gianlazzarini@gmail.com>
2016-08-14 00:41:31 -0700

    Gets intent string and sets it to textView
    http://stackoverflow.com/questions/29500897/pass-rowid-in-listview-to-new-activity-when-clicked

--- app/src/main/java/com/lazztech/thoughtlog/AllDBRow.java
+++ app/src/main/java/com/lazztech/thoughtlog/AllDBRow.java
@@ -4,6 +4,7 @@ import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
 import android.widget.TextView;
 
+
 /**
  * Created by gianlazzarini on 8/13/16.
  */
@@ -26,8 +27,16 @@ public class AllDBRow extends AppCompatActivity {
         //Set MainActivity.xml as user interface layout
         setContentView(R.layout.alldbrow);
         // bind GUI elements with local controls
+        DatabaseHelper myDb;
 
-        String data = getIntent().getExtras().getString("_id");
+        String iddata = getIntent().getExtras().getString("id");
+        String situationdata = getIntent().getExtras().getString("situation");
+        String thoughtsdata = getIntent().getExtras().getString("thoughts");
+        String emotionsdata = getIntent().getExtras().getString("emotions");
+        String behaviordata = getIntent().getExtras().getString("behaviors");
+        String distortionsdata = getIntent().getExtras().getString("distortions");
+        String altbehaviordata = getIntent().getExtras().getString("altbehaviors");
+        String altthoughtsdata = getIntent().getExtras().getString("altthoughts");
 
         id = (TextView) findViewById(R.id.dbidTextView);
         situation = (TextView) findViewById(R.id.situationTextView);
@@ -37,6 +46,15 @@ public class AllDBRow extends AppCompatActivity {
         distortions = (TextView) findViewById(R.id.distortionsTextView);
         altbehavior = (TextView) findViewById(R.id.altbehaviorTextView);
         altthoughts = (TextView) findViewById(R.id.altthoughsTextView);
+
+        id.setText(iddata);
+        situation.setText(situationdata);
+        thoughts.setText(thoughtsdata);
+        emotions.setText(emotionsdata);
+        behavior.setText(behaviordata);
+        distortions.setText(distortionsdata);
+        altbehavior.setText(altbehaviordata);
+        altthoughts.setText(altthoughtsdata);
 
     }
 }
