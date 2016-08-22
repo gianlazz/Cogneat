888a84eabae65b6c6109f0abcae096e16552e015
Gian Lazzarini <gianlazzarini@gmail.com>
2016-08-21 17:01:14 -0700

    Implemented the update method

--- app/src/main/java/com/lazztech/thoughtlog/AllDBRow.java
+++ app/src/main/java/com/lazztech/thoughtlog/AllDBRow.java
@@ -2,7 +2,11 @@ package com.lazztech.thoughtlog;
 
 import android.os.Bundle;
 import android.support.v7.app.AppCompatActivity;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 
 /**
@@ -13,13 +17,17 @@ public class AllDBRow extends AppCompatActivity {
     /** Called when the activity is first created. */
 
     TextView id;
-    TextView situation;
-    TextView thoughts;
-    TextView emotions;
-    TextView behavior;
-    TextView distortions;
-    TextView altbehavior;
-    TextView altthoughts;
+    TextView datetime;
+    EditText situation;
+    EditText thoughts;
+    EditText emotions;
+    EditText behavior;
+    EditText distortions;
+    EditText altbehavior;
+    EditText altthoughts;
+    Button updatebtn;
+
+    DatabaseHelper myDb;
 
     @Override
     public void onCreate(Bundle savedInstanceState){
@@ -27,9 +35,10 @@ public class AllDBRow extends AppCompatActivity {
         //Set MainActivity.xml as user interface layout
         setContentView(R.layout.alldbrow);
         // bind GUI elements with local controls
-        DatabaseHelper myDb;
 
+        myDb = new DatabaseHelper(this);
         String iddata = getIntent().getExtras().getString("id");
+        String datetimedata = getIntent().getExtras().getString("datetime");
         String situationdata = getIntent().getExtras().getString("situation");
         String thoughtsdata = getIntent().getExtras().getString("thoughts");
         String emotionsdata = getIntent().getExtras().getString("emotions");
@@ -39,22 +48,49 @@ public class AllDBRow extends AppCompatActivity {
         String altthoughtsdata = getIntent().getExtras().getString("altthoughts");
 
         id = (TextView) findViewById(R.id.dbidTextView);
-        situation = (TextView) findViewById(R.id.situationTextView);
-        thoughts = (TextView) findViewById(R.id.thoughtsTextView);
-        emotions = (TextView) findViewById(R.id.emotionsTextView);
-        behavior = (TextView) findViewById(R.id.behaviorTextView);
-        distortions = (TextView) findViewById(R.id.distortionsTextView);
-        altbehavior = (TextView) findViewById(R.id.altbehaviorTextView);
-        altthoughts = (TextView) findViewById(R.id.altthoughsTextView);
+        datetime = (TextView) findViewById(R.id.datetimeTextView);
+        situation = (EditText) findViewById(R.id.situationTextView);
+        thoughts = (EditText) findViewById(R.id.thoughtsTextView);
+        emotions = (EditText) findViewById(R.id.emotionsTextView);
+        behavior = (EditText) findViewById(R.id.behaviorTextView);
+        distortions = (EditText) findViewById(R.id.distortionsTextView);
+        altbehavior = (EditText) findViewById(R.id.altbehaviorTextView);
+        altthoughts = (EditText) findViewById(R.id.altthoughsTextView);
+        updatebtn = (Button) findViewById(R.id.updateButton);
 
-        id.setText("ID: " + iddata);
-        situation.setText("Situation: " + situationdata);
-        thoughts.setText("Thoughts: " + thoughtsdata);
-        emotions.setText("Emotions: " + emotionsdata);
-        behavior.setText("Behavior: " + behaviordata);
-        distortions.setText("Distortions: " + distortionsdata);
-        altbehavior.setText("Alt Behavior: " + altbehaviordata);
-        altthoughts.setText("Alt Thoughts: " +  altthoughtsdata);
+        id.setText(iddata);
+        datetime.setText(datetimedata);
+        situation.setText(situationdata);
+        thoughts.setText(thoughtsdata);
+        emotions.setText(emotionsdata);
+        behavior.setText(behaviordata);
+        distortions.setText(distortionsdata);
+        altbehavior.setText(altbehaviordata);
+        altthoughts.setText(altthoughtsdata);
 
+        UpdateDate();
+    }
+
+    public void UpdateDate(){
+        updatebtn.setOnClickListener(
+                new View.OnClickListener() {
+                    @Override
+                    public void onClick(View v) {
+                        boolean isUpdated = myDb.updateData(id.getText().toString(),
+                                datetime.getText().toString(),
+                                situation.getText().toString(),
+                                thoughts.getText().toString(),
+                                emotions.getText().toString(),
+                                behavior.getText().toString(),
+                                distortions.getText().toString(),
+                                altbehavior.getText().toString(),
+                                altthoughts.getText().toString());
+                        if(isUpdated == true)
+                            Toast.makeText(AllDBRow.this, "Data Updated", Toast.LENGTH_LONG).show();
+                        else
+                            Toast.makeText(AllDBRow.this, "Data Not Updated!", Toast.LENGTH_LONG).show();
+                    }
+                }
+        );
     }
 }
